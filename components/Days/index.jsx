343891dcc57d51961a0641e5d8e01b192443a62a
woodygaragelab <woodygaragelab@gmail.com>
2021-10-23 11:12:55 +0900

    v0.9

---
+++ components/Days/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Days.css";
+
+function Days(props) {
+  const { className } = props;
+
+  return (
+    <div className={`days ${className || ""}`}>
+      <div className="overlap-group-42">
+        <div className="yyyymmdd sfcompact-regular-normal-new-york-pink-20px">YYYY/MM/DD</div>
+        <img className="x9-1" src="/img/file---10@1x.png" />
+        <img className="x10-1" src="/img/file---10@1x.png" />
+      </div>
+    </div>
+  );
+}
+
+export default Days;
