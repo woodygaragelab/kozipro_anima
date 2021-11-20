99ed0cbbf05297f205acfaa4a9e09354f127b617
OsakiMiku <miku.osaki@ibm.com>
2021-11-20 10:18:25 +0900

    osaki 変更

--- components/DetailedHeader/index.jsx
+++ components/DetailedHeader/index.jsx
@@ -5,7 +5,7 @@ function DetailedHeader() {
   return (
     <div className="detailed-header border-1px-my-pink">
       <div className="x6">
-        <div className="kozipro sfpro-bold-black-20px">Kozipro 1120</div>
+        <div className="kozipro sfpro-bold-black-20px">Kozipro osaki</div>
       </div>
     </div>
   );
